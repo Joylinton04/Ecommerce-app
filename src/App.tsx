fd7e7134eed5daaacce353ffea490d3bc7d5b967
Joylinton04 <adujoy05@gmail.com>
2025-08-04 20:31:08 +0000

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { RouterProvider } from 'react-router';
 import Approute from './route/Approute';
 
+
 const App = () => {
   return <RouterProvider router={Approute}/>
 }
