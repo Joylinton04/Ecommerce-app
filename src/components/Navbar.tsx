b8f947d93b06574413ec48f4d9fe1202f1c53fff
Joylinton04 <adujoy05@gmail.com>
2025-08-04 20:31:53 +0000

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { ShoppingCart, User } from 'lucide-react'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    const navLinks = [
+        { path: "/", to: 'Home' },
+        { path: "/categories", to: 'Categories' },
+        { path: "/about", to: 'About' },
+        { path: "/contact", to: 'Contact' },
+    ]
+
+    return (
+        <div className='max-w-[1440px] mx-auto px-14 py-4 flex items-center justify-between font-body'>
+            <h1 className='font-heading text-3xl font-semibold italic cursor-pointer'>ECOM</h1>
+            <div className='flex items-center gap-4'>
+                {navLinks.map((Link, index) => (
+                    <NavLink
+                        className="transition-all duration-300 py-1 hover:px-2 hover:bg-black hover:text-white rounded-full"
+                        to={Link.path} key={index}>{Link.to}
+                    </NavLink>
+                ))}
+            </div>
+            <div className='flex items-center gap-2'>
+                <span className='p-5 h-6 w-6 hover:bg-black transition-all duration-300 grid place-content-center rounded-full hover:text-white'><User className='hover:text-white'/></span>
+                <span className='p-5 h-6 w-6 hover:bg-black transition-all duration-300 grid place-content-center rounded-full hover:text-white'><ShoppingCart className='hover:text-white' /></span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
