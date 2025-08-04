f00ab3fb42d8e3688f94abf50dfac4bc46a2fe90
Joylinton04 <adujoy05@gmail.com>
2025-08-04 20:30:55 +0000

    assets

---
+++ src/assets/assets.ts
@@ -0,0 +1,19 @@
+import roundNeck from './Man with Beard Photo.avif';
+import whiteTShirt from './White T-Shirt Product Image.avif'
+import jeans1 from './Crawford Jolly Pant Picture.jpg'
+import jeans2 from './Denim Bottoms Photo.jpg'
+import airForce from './Shoe Picture from Unsplash (1).jpg'
+import puma from './Shoe Picture from Unsplash.jpg'
+import headerImg from './Nike Air Force 1 High.jpg'
+
+const assets = {
+  roundNeck,
+  whiteTShirt,
+  jeans1,
+  jeans2,
+  airForce,
+  puma,
+  headerImg
+}
+
+export default assets;
