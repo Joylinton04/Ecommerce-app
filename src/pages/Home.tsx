001ebb9cb6c30a120213a8819611d1a38a90eb3c
Joylinton04 <adujoy05@gmail.com>
2025-08-05 20:26:46 +0000

    accordian section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,99 @@
-import React from 'react'
-import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import ProductTitle from '@/components/ProductTitle';
-import assets from '@/assets/assets';
-import ProductCard from '@/components/ProductCard';
-import Navbar from '@/components/Navbar';
+import React from "react";
+import ProductTitle from "@/components/ProductTitle";
+import assets from "@/assets/assets";
+import ProductCard from "@/components/ProductCard";
+import Navbar from "@/components/Navbar";
+import AccordianSec from "@/components/AccordianSec";
 
 const Home = () => {
+  const latestProduct = [
+    {
+      img: assets.whiteTShirt,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.airForce,
+      price: 49.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.roundNeck,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.jeans1,
+      price: 59.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.jeans2,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.whiteTShirt,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.roundNeck,
+      price: 19.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.puma,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+  ];
+  const Bestsellers = [
+    {
+      img: assets.blackTop,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.hoodie,
+      price: 49.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.whiteT,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.topPavillion,
+      price: 59.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.sweater,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.greenTop,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.topPavillion,
+      price: 19.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+    {
+      img: assets.blackTop,
+      price: 29.99,
+      text: "Men round neck pure cotten t-shirt",
+    },
+  ];
+
   return (
-    <div className='min-h-screen w-full font-body'>
-      <Navbar/>
+    <div className="h-[500vh] w-full font-body">
+      <Navbar />
       {/* section 1 */}
       <div className="h-[672px] bg-gradient-to-tl from-cyan-500/30">
         <div className="max-w-[1440px] mx-auto flex items-center justify-start h-full p-16 lgg:p-10">
@@ -27,8 +111,8 @@ const Home = () => {
                 </div>
                 <p className="text-lg leading-relaxed lgg:text-base">
                   Explore our best-selling products and discover why everyone is
-                  talking about them. This is a limited-time opportunity to get your
-                  favorites at an incredible price.
+                  talking about them. This is a limited-time opportunity to get
+                  your favorites at an incredible price.
                 </p>
               </div>
               <button className="flex items-center gap-6 px-6 py-3 bg-white text-black font-semibold rounded-full transition-colors hover:bg-gray-200 w-fit mt-4">
@@ -60,52 +144,78 @@ const Home = () => {
       </div>
 
       {/* Latest Arrivals */}
-      <div className='flex flex-col px-14 py-4 max-w-[1440px] mx-auto ssm:px-7'>
+      <div className="flex flex-col px-14 py-4 max-w-[1440px] mx-auto ssm:px-7">
         <ProductTitle
           title="LATEST ARRIVAL"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias ipsum esse nobis aliquam dolores eveniet eaque necessitatibus ex voluptates. Delectus quam enim, dolor" />
-        <div className='flex flex-wrap gap-12 mt-10'>
-          <ProductCard
-            img={assets.roundNeck}
-            price={49.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.whiteTShirt}
-            price={29.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.jeans2}
-            price={49.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.puma}
-            price={59.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.whiteTShirt}
-            price={59.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.jeans1}
-            price={59.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
-          <ProductCard
-            img={assets.airForce}
-            price={59.99}
-            text={"Men round neck pure cotten t-shirt"}
-          />
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias ipsum esse nobis aliquam dolores eveniet eaque necessitatibus ex voluptates. Delectus quam enim, dolor"
+        />
+        <div className="flex flex-wrap gap-12 mt-10">
+          {latestProduct.map((product, index) => (
+            <ProductCard
+              key={index}
+              img={product.img}
+              price={product.price}
+              text={product.text}
+            />
+          ))}
         </div>
       </div>
 
-      {/*  */}
+      {/* Best seller */}
+      <div className="flex flex-col px-14 py-4 max-w-[1440px] mx-auto ssm:px-7 mt-20">
+        <ProductTitle
+          title="BEST SELLER"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias ipsum esse nobis aliquam dolores eveniet eaque necessitatibus ex voluptates. Delectus quam enim, dolor"
+        />
+        <div className="flex flex-wrap gap-12 mt-10">
+          {Bestsellers.map((product, index) => (
+            <ProductCard
+              key={index}
+              img={product.img}
+              price={product.price}
+              text={product.text}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* section 3 */}
+      <div className="mt-20 bg-[#B6B09F]">
+        <div className="px-14 py-6 max-w-[1440px] mx-auto ssm:px-7 flex gap-4 justify-between">
+          <div className="flex flex-col gap-1">
+            <h1 className="font-bold font-heading text-8xl">50+</h1>
+            <p className="text-sm">Trusted Customers</p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="font-bold font-heading text-8xl">99%</h1>
+            <p className="text-sm text-center">Assured of secured payments</p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="font-bold font-heading text-8xl">2K</h1>
+            <p className="text-sm">Customers Worldwide</p>
+          </div>
+        </div>
+      </div>
+
+      {/* FAQ */}
+      <div className="mt-20 px-14 py-4 max-w-[1440px] mx-auto ssm:px-7">
+        <ProductTitle
+          title="FREQUENTLY ASKED QUESTIONS"
+          text="Got questions? We've got the answers you need right here!"
+        />
+        <div className="mt-6 flex gap-6">
+          <div className="h-[600px]">
+            <img
+              src={assets.fqaimg}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+
+          <AccordianSec />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
