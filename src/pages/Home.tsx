8c65e903d06e20289c81858e9a63e472e60f234e
Joylinton04 <adujoy05@gmail.com>
2025-08-06 19:28:59 +0000

    customer review section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -214,6 +214,67 @@ const Home = () => {
           <AccordianSec />
         </div>
       </div>
+
+      {/* customer review */}
+      <div className="mt-20 px-14 py-4 max-w-[1440px] mx-auto ssm:px-7">
+        <ProductTitle
+          title="CUSTOMERS REVIEW"
+          text="See what our valued customers are saying about their experiences!"
+        />
+        <div className="flex flex-col gap-20 mt-10">
+          <div className="flex gap-4">
+            <img src={assets.fqa1} className="w-[251px]" />
+            <div className="flex flex-col gap-4 justify-between">
+              <p className="text-xl">
+                Fast shipping and great packaging! My order arrived in perfect
+                condition.
+              </p>
+              <div className="flex gap-3">
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+              </div>
+              <span className='block h-1 bg-black'></span>
+              <h1 className="font-bold text-xl">Mariana Van Zeeler</h1>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <img src={assets.fqa2} className="w-[251px]" />
+            <div className="flex flex-col gap-4 justify-between">
+              <p className="text-xl">
+                Fast shipping and great packaging! My order arrived in perfect
+                condition.
+              </p>
+              <div className="flex gap-3">
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+              </div>
+              <span className='block h-1 bg-black'></span>
+              <h1 className="font-bold text-xl">Mariana Van Zeeler</h1>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <img src={assets.fqa3} className="w-[251px]" />
+            <div className="flex flex-col gap-4 justify-between">
+              <p className="text-xl">
+                Fast shipping and great packaging! My order arrived in perfect
+                condition.
+              </p>
+              <div className="flex gap-3">
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+                <img src={assets.star} className="w-10"/>
+              </div>
+              <span className='block h-1 bg-black'></span>
+              <h1 className="font-bold text-xl">Mariana Van Zeeler</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
