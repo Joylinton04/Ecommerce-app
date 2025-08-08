450fcecd8805ca2b1dc84a14aae7b13eebec65f3
Joylinton04 <adujoy05@gmail.com>
2025-08-08 22:29:51 +0000

    app layout

--- src/route/Approute.tsx
+++ src/route/Approute.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Home from '@/pages/Home';
+import React from "react";
+import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "@/layout/Layout";
 
 const Approute = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 export default Approute;
