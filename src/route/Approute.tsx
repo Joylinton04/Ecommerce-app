e4c5491e1e83a9b97d949937a0e858fc09bc1d79
Joylinton04 <adujoy05@gmail.com>
2025-08-09 22:39:10 +0000

    categories page route

--- src/route/Approute.tsx
+++ src/route/Approute.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/layout/Layout";
+import Categories from "@/pages/Categories";
 
 const Approute = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const Approute = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: 'categories',
+        element: <Categories/>,
       },
     ],
   },
