48363528ed5def8b3a42c8be225d7affa9c00514
Joylinton04 <adujoy05@gmail.com>
2025-08-10 00:56:11 +0000

    navbar refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="max-w-[1440px] mx-auto px-14 py-4 flex items-center justify-between font-body ssm:px-6">
+    <div className="max-w-[1440px] mx-auto px-14 py-4 flex items-center justify-between font-body lgg:px-6 border-b">
       <h1 className="font-heading text-3xl font-semibold italic cursor-pointer">
         ECOM
       </h1>
