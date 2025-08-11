ede3bcc7dfdcdcaaa1a5ebaa3dc7c55713f6f36b
Joylinton04 <adujoy05@gmail.com>
2025-08-11 23:30:06 +0000

    navbar responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="max-w-[1440px] mx-auto px-14 py-4 flex items-center justify-between font-body lgg:px-6 border-b">
       <h1 className="font-heading text-3xl font-semibold italic cursor-pointer">
-        ECOM
+        <a href="/">ECOM</a>
       </h1>
       <div className="flex items-center gap-4 ssm:hidden">
         {navLinks.map((Link, index) => (
