8746300bd8c5f27f00b67b2cfa14801791fb313a
Joylinton04 <adujoy05@gmail.com>
2025-08-11 23:30:20 +0000

    comp refactor

--- src/components/ProductTitle.tsx
+++ src/components/ProductTitle.tsx
@@ -12,7 +12,7 @@ const ProductTitle = ({ title, text }: prop) => {
                 <h1 className='font-heading font-bold text-2xl ssm:text-lg ssm:pb-2'>{title}</h1>
                 <span className='block h-1 w-20 bg-black'></span>
             </div>
-            <p className='font-body text-sm text-gray-600 ssm:text-center'>{text}</p>
+            <p className='font-body text-sm text-gray-600 ssm:text-start'>{text}</p>
         </div>
     )
 }
