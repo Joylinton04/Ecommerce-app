fb4ccd096f2725a089b93438a7583c8991dec5dc
Joylinton04 <adujoy05@gmail.com>
2025-08-14 18:34:00 +0000

    added product page and route

--- src/route/Approute.tsx
+++ src/route/Approute.tsx
@@ -3,6 +3,7 @@ import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/layout/Layout";
 import Categories from "@/pages/Categories";
+import Product from "@/pages/Product";
 
 const Approute = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const Approute = createBrowserRouter([
       {
         path: 'categories',
         element: <Categories/>,
+      },
+      {
+        path: 'product/:id',
+        element: <Product/>,
       },
     ],
   },
