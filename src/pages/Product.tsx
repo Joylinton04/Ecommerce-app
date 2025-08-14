8c1e4d227abf29c70603fb6368cb4b10160a7490
Joylinton04 <adujoy05@gmail.com>
2025-08-14 18:33:44 +0000

    product page

---
+++ src/pages/Product.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='max-w-[1440px] p-14 min-h-screen'>
+        <div className='flex gap-6'>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Product;
