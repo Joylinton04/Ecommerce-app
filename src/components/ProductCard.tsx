fbd441b200ef36ee5a491b8e9119779713b59521
Joylinton04 <adujoy05@gmail.com>
2025-08-26 09:34:47 +0000

    product card refactor

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,10 +10,10 @@ interface prop {
 
 const ProductCard = ({img, title, price, id}: prop) => {
     return (
-        <Link to={`/product/${id}`} className='flex flex-col cursor-pointer'>
-            <img src={img} className='w-[260px] h-[300px] ssm:w-[300px] object-cover object-center'/>
-            <p className='capitalize py-1'>{title}</p>
-            <h3 className='font-bold'>${price}</h3>
+        <Link to={`/product/${id}`} className='flex flex-col cursor-pointer ssm:p-2 ssm:bg-white w-[250px] ssm:w-[160px]'>
+            <img src={img} className=' w-full ssm:h-[240px] h-[300px] object-cover object-center'/>
+            <p className='capitalize py-1 ssm:text-sm'>{title}</p>
+            <h3 className='font-bold ssm:text-sm'>${price}</h3>
         </Link>
     )
 }
