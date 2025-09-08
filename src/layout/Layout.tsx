8e7ef8e8286f6970b89e0957f8c1f4d60641c6c6
Joylinton04 <adujoy05@gmail.com>
2025-09-08 22:01:35 +0000

    scroll to top feature

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,27 @@
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const Layout = () => {
   return (
-    <div className='min-h-screen'>
-        <Navbar />
-        <Outlet/>
-        <Footer/>
+    <div className="min-h-screen">
+      <ScrollToTop />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
