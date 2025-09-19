fc320cd5ff8ea6d1df8d049244f04f2c9c95e61c
Joylinton04 <adujoy05@gmail.com>
2025-09-19 23:04:54 +0000

    navbar responsiveness

--- admin/src/components/ui/Navbar.tsx
+++ admin/src/components/ui/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./button";
 const Navbar = () => {
   return (
     <div className="font-body border-b">
-      <div className="max-w-[1440px] mx-auto px-14 py-2 flex justify-between items-center gap-2">
+      <div className="max-w-[1440px] mx-auto px-14 py-2 flex justify-between items-center gap-2 ssm:px-7">
         <div className="flex flex-col">
           <h1 className="italic font-bold text-3xl">ECOM</h1>
           <span className="uppercase text-sm">admin panel</span>
