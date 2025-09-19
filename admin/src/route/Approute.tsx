fa9445046708b44e890b1ac680924ccff7735193
Joylinton04 <adujoy05@gmail.com>
2025-09-19 23:05:54 +0000

    layout

--- admin/src/route/Approute.tsx
+++ admin/src/route/Approute.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
+import Listitems from '@/pages/Listitems';
+import Layout from '@/pages/Layout';
 
 const Approute = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>
-    }
+        element: <Layout/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: '/list-items',
+                element: <Listitems/>
+            }
+        ]
+    },
 ])
 
 export default Approute;
